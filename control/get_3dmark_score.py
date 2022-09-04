30d20596f76672bfbc6cd8a39ad695d3e299665b
wenyan <wenyan@qiniu.com>
2022-09-04 19:36:36 +0800

    print score test1 test2 resule

--- control/get_3dmark_score.py
+++ control/get_3dmark_score.py
@@ -1,4 +1,5 @@
 import os
+from pickle import FALSE, TRUE
 import sys
 import tempfile
 import zipfile
@@ -36,45 +37,74 @@ def extract_3dResult(filename,dst_name):
 
 def read_xml(file_path,item):
     data=float()
+    find_it=bool()
+    find_it= False
     dom = ElementTree.parse(file_path)
     root = dom.getroot()
     result = root.findall(".//result/")
     for element in result:
         if element.tag == item:
             data= element.text
+            find_it = True
             break
 
+    if find_it == False:
+        return -1 # -1 means not found 
     return data
 
 
 def get_3dmark_score(rootDir,dstFile,data):
     fileList=get_3dmark(rootDir)
-    score=float()
-    if data[1]=="TimeSpy_Score":
-        for file in fileList:
-            if "TimeSpy" in file:
-                timespy_log=extract_3dResult(file, os.path.join(rootDir,"3dmarkResult-{}.xml".format(common.get_time())))
+    score, test1, test2=float(),float(),float()
+    for file in fileList:
+        if "TimeSpy" in file:
+            timespy_log=extract_3dResult(file, os.path.join(rootDir,"3dmarkResult-{}.xml".format(common.get_time())))
+            if data[1]=="TimeSpy_Score" or data[1] == "TimeSpy_FPS":
                 score=read_xml(timespy_log,"TimeSpyExtremeCustomGraphicsScore")
-                break
-    elif data[1] == "TimeSpy_FPS":
-        for file in fileList:
-            if "TimeSpy" in file:
-                timespy_log = extract_3dResult(file, os.path.join(rootDir,
-                                                                  "3dmarkResult-{}.xml".format(common.get_time())))
-                score = read_xml(timespy_log, "TimeSpyExtremeCustomGraphicsScore1")
-                # TODO 再增加判断
-                if score == 0:
-                    score=read_xml(timespy_log,"TimeSpyCustomGraphicsTest2")
+                test1=read_xml(timespy_log,"TimeSpyCustomGraphicsTest1")
+                test2=read_xml(timespy_log,"TimeSpyCustomGraphicsTest2")
+                print(score, test1, test2)
+                if score != 0:
+                    print("It is TimeSpy_Score")
+                elif score == 0 & test1 == -1 & test2 !=0:
+                    print("It is TimeSpy_FPS")
+                else:
+                    print("Neither TimeSpy_Score and nor TimeSpy_FPS")
+        elif "FireStrike" in file:
+                if data[1] in file:
+                    timespy_log=extract_3dResult(file, os.path.join(rootDir,"3dmarkResult-{}.xml".format(common.get_time())))
+                    score=read_xml(timespy_log,"FireStrikeCustomGraphicsScore")
                     break
+        else:
+            raise Exception("Parameter error")
 
-    elif data[1]=="FireStrike":
-        for file in fileList:
-            if data[1] in file:
-                timespy_log=extract_3dResult(file, os.path.join(rootDir,"3dmarkResult-{}.xml".format(common.get_time())))
-                score=read_xml(timespy_log,"FireStrikeCustomGraphicsScore")
-                break
-    else:
-        raise Exception("Parameter error")
+    # if data[1]=="TimeSpy_Score":
+    #     for file in fileList:
+    #         if "TimeSpy" in file:
+    #             timespy_log=extract_3dResult(file, os.path.join(rootDir,"3dmarkResult-{}.xml".format(common.get_time())))
+    #             score=read_xml(timespy_log,"TimeSpyExtremeCustomGraphicsScore")
+    #             if score != 0:
+    #                 print("have score")
+    #             break
+    # elif data[1] == "TimeSpy_FPS":
+    #     for file in fileList:
+    #         if "TimeSpy" in file:
+    #             timespy_log = extract_3dResult(file, os.path.join(rootDir,
+    #                                                               "3dmarkResult-{}.xml".format(common.get_time())))
+    #             score = read_xml(timespy_log, "TimeSpyExtremeCustomGraphicsScore1")
+    #             # TODO 再增加判断
+    #             if score == 0 & TimeSpyCustomGraphicsTest1 == NULL & TimeSpyCustomGraphicsTest2 !=0:
+    #                 score=read_xml(timespy_log,"TimeSpyCustomGraphicsTest2")
+    #                 break
+
+    # elif data[1]=="FireStrike":
+    #     for file in fileList:
+    #         if data[1] in file:
+    #             timespy_log=extract_3dResult(file, os.path.join(rootDir,"3dmarkResult-{}.xml".format(common.get_time())))
+    #             score=read_xml(timespy_log,"FireStrikeCustomGraphicsScore")
+    #             break
+    # else:
+    #     raise Exception("Parameter error")
     return write2excel(dstFile, "sheet1", [data[0], data[1], float(score)])
 
 def get_3dmark11(rootDir):
